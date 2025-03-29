50bdc6c0eba3ea45cd5077d3e1acb5f037b5e203
Ty Bekiares <tyrone.bekiares@elastic.co>
2025-03-29 15:44:10 -0500

    update

--- src/build.sh
+++ src/build.sh
@@ -1,20 +1,20 @@
-#arch=linux/amd64
-arch=linux/arm64
 
-for dir in *; do
-  if [[ -d "$dir" ]]; then
-    cd "$dir"
-    docker build --platform $arch --progress plain -t us-central1-docker.pkg.dev/elastic-sa/tbekiares/$dir .
-    #docker push us-central1-docker.pkg.dev/elastic-sa/tbekiares/$dir:latest
-    cd ..
-  fi
-done
-
+# arch=linux/arm64
 # for dir in *; do
 #   if [[ -d "$dir" ]]; then
 #     cd "$dir"
 #     docker build --platform $arch --progress plain -t us-central1-docker.pkg.dev/elastic-sa/tbekiares/$dir .
-#     docker push us-central1-docker.pkg.dev/elastic-sa/tbekiares/$dir:latest
+#     #docker push us-central1-docker.pkg.dev/elastic-sa/tbekiares/$dir:latest
 #     cd ..
 #   fi
 # done
+
+arch=linux/amd64
+for dir in *; do
+  if [[ -d "$dir" ]]; then
+    cd "$dir"
+    docker build --platform $arch --progress plain -t us-central1-docker.pkg.dev/elastic-sa/tbekiares/$dir .
+    docker push us-central1-docker.pkg.dev/elastic-sa/tbekiares/$dir:latest
+    cd ..
+  fi
+done
