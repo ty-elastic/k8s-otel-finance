192e4c6f4112901d7836d026128efc9edb407bf9
Ty Bekiares <tyrone.bekiares@elastic.co>
2025-06-19 22:03:32 -0500

    ty

--- rebuild.sh
+++ rebuild.sh
@@ -14,7 +14,7 @@ echo $service
 echo $COURSE
 echo $IMAGE_VERSION
 
-docker build --platform $arch --build-arg COURSE=$COURSE --progress plain -t us-central1-docker.pkg.dev/elastic-sa/tbekiares/$service:$IMAGE_VERSION src/$service
+docker build --platform $arch --build-arg VARIANTS=false --build-arg COURSE=$COURSE --progress plain -t us-central1-docker.pkg.dev/elastic-sa/tbekiares/$service:$IMAGE_VERSION src/$service
 #docker push us-central1-docker.pkg.dev/elastic-sa/tbekiares/$service:$IMAGE_VERSION
 
 sed "s,#imagePullPolicy,imagePullPolicy,g" k8s/$service.yaml | envsubst | kubectl apply -f -
