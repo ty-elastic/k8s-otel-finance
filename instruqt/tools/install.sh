aeb9ba21d047c7e2166e4ce5cd9c63028ca1232b
Ty Bekiares <tyrone.bekiares@elastic.co>
2025-06-23 10:38:28 -0500

    ty

--- instruqt/tools/install.sh
+++ instruqt/tools/install.sh
@@ -7,12 +7,19 @@ for dir in /workspace/workshop/src/*/; do
   if [[ -d "$dir" ]]; then
     service=$(basename "$dir")
     echo $service
-    if [ -d "$dir/variants/$INSTRUQT_TRACK_SLUG" ]; then
+    if [ -d "$dir/variants/$COURSE" ]; then
         echo "patching"
         cd /workspace/workshop/src/$service
-        patch < variants/$INSTRUQT_TRACK_SLUG/init.patch
+        patch < variants/$COURSE/init.patch
     fi
   fi
 done
+
 cd /workspace/workshop
+
+if [ -d "k8s/variants/$COURSE" ]; then \
+    echo $COURSE; \
+    patch < variants/$COURSE/init.patch
+fi
+
 ./install.sh
