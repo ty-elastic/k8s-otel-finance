03546b09deac140d662982479c69cb2d3a875a06
Ty Bekiares <tyrone.bekiares@elastic.co>
2025-06-23 20:02:45 -0500

    Ty

--- instruqt/update.sh
+++ instruqt/update.sh
@@ -13,26 +13,27 @@ done
 # git push origin main
 
 for dir in ./courses/*/; do
+  echo $dir
   if [[ -d "$dir" ]]; then
     COURSE=$(basename "$dir")
     echo $COURSE
 
     if [ "$build" = "true" ]; then
-      for dir in ../src/*/; do
-          echo $dir
-          if [[ -d "$dir" ]]; then
-              service=$(basename "$dir")
+      for service_dir in ../src/*/; do
+          echo $service_dir
+          if [[ -d "$service_dir" ]]; then
+              service=$(basename "$service_dir")
               echo $service
               echo $COURSE
-              docker build --platform $arch --build-arg COURSE=$COURSE --progress plain -t us-central1-docker.pkg.dev/elastic-sa/tbekiares/$service:$COURSE $dir
+              docker build --platform $arch --build-arg COURSE=$COURSE --progress plain -t us-central1-docker.pkg.dev/elastic-sa/tbekiares/$service:$COURSE $service_dir
               docker push us-central1-docker.pkg.dev/elastic-sa/tbekiares/$service:$COURSE
           fi
       done
     fi
 
-    cd ./courses/$COURSE
+    cd courses/$COURSE
     instruqt track push --force
-    cd ..
+    cd ../..
   fi
 done
 
