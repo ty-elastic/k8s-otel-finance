f15c27864cd495f77cdd9c3c390bc5fc3355f0a9
Ty Bekiares <tyrone.bekiares@elastic.co>
2025-06-26 11:35:15 -0600

    ty

--- collector/patch.sh
+++
@@ -1,5 +0,0 @@
-curl -o values.yaml https://raw.githubusercontent.com/elastic/elastic-agent/refs/tags/v8.17.4/deploy/helm/edot-collector/kube-stack/values.yaml
-
-patch -u values.yaml -i values.patch
-
-helm upgrade -f values.yaml opentelemetry-kube-stack  open-telemetry/opentelemetry-kube-stack --namespace opentelemetry-operator-system
